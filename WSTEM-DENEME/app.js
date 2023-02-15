c75bb51f8d94ac06ff6f8cabeea2fc39022bdd33
AlperenKonukbay <93219481+AlperenKonukbay@users.noreply.github.com>
2023-02-15 22:32:31 +0300

    Add files via upload

---
+++ WSTEM-DENEME/app.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  let headings = document.querySelectorAll('article h2');
+  for (let i = 0; i < headings.length; i++) {
+    if (i == n - 1) {
+      headings[i].classList.add('active');
+    } else {
+      headings[i].classList.remove('active');
+    }
+  }
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
